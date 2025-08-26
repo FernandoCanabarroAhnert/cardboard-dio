0f4c9d111e4358082174f16745b3824c2b594d1c
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2025-08-26 17:28:52 -0300

    init: primeiro commit

---
+++ src/main/java/com/fernandocanabarro/cardboard_dio/CardboardDioApplication.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.cardboard_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardboardDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardboardDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandocanabarro/cardboard_dio/CardboardDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandocanabarro.cardboard_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardboardDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
